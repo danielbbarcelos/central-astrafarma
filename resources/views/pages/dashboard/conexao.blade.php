c7b6e084c3f1b611846a06172521e7d8f77e110f
Daniel Barcelos <daniel.barcelos@2mind.com.br>
2019-05-17 12:56:05 -0300

    Ocultação de gráficos estáticos do dashboard

---
+++ resources/views/pages/dashboard/conexao.blade.php
@@ -0,0 +1,34 @@
+<div class="col s12 m12 l4">
+    <div class="card server-card">
+        <div class="card-content">
+            <div class="card-options">
+                <ul>
+                    <li class="red-text"><span class="badge blue-grey lighten-3">optimal</span></li>
+                </ul>
+            </div>
+            <span class="card-title">Server Load</span>
+            <div class="server-load row">
+                <div class="server-stat col s4">
+                    <p>167GB</p>
+                    <span>Usage</span>
+                </div>
+                <div class="server-stat col s4">
+                    <p>320GB</p>
+                    <span>Space</span>
+                </div>
+                <div class="server-stat col s4">
+                    <p>57.4%</p>
+                    <span>CPU</span>
+                </div>
+            </div>
+            <div class="stats-info">
+                <ul>
+                    <li>Google Chrome<div class="percent-info green-text right">32% <i class="material-icons">trending_up</i></div></li>
+                    <li>Safari<div class="percent-info red-text right">20% <i class="material-icons">trending_down</i></div></li>
+                    <li>Mozilla Firefox<div class="percent-info green-text right">18% <i class="material-icons">trending_up</i></div></li>
+                </ul>
+            </div>
+            <div id="flotchart2"></div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/pages/dashboard/pedido-venda.blade.php
@@ -0,0 +1,13 @@
+<div class="col s12 m12 l8">
+    <div class="card visitors-card">
+        <div class="card-content">
+            <div class="card-options">
+                <ul>
+                    <li><a href="javascript:void(0)" class="card-refresh"><i class="material-icons">refresh</i></a></li>
+                </ul>
+            </div>
+            <span class="card-title">Visitors<br><span class="secondary-title">Showing stats from the last week</span></span>
+            <div id="flotchart1"></div>
+        </div>
+    </div>
+</div>
